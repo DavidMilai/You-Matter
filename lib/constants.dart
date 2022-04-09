b0b8d2c9e3deae535e8c7d839425091eedc4cf8a
DavidMilai <davidmilai67@gmail.com>
2022-04-09 18:11:33 +0300

    abi

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+String kInfuraUrl =
+    "https://rinkeby.infura.io/v3/2853eac586314cb9842faddb8c6b649c";
+String kSocketUrl =
+    "wss://rinkeby.infura.io/ws/v3/2853eac586314cb9842faddb8c6b649c";
+
+String kPrivateKey =
+    "01b3b3c4a853d48d46a6222a59394bd6bcf0bd53e6ef2fa18a2ee39af903f809";
+
+String kMyAddress = '0x7f0e27D1393d72ef5b298Af2219E11E64C974854';
+// String kContractAddress = '0x77B494282d7684d356806aEC861061F19986c7D2';
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,8 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:web3dart/web3dart.dart';
+
+import '../constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,6 +15,16 @@ class _HomeScreenState extends State<HomeScreen> {
   Client? httpClient;
   Web3Client? web3Client;
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    httpClient = Client();
+    web3Client = Web3Client(kInfuraUrl, httpClient!);
+    // web3Client = Web3Client(kInfuraUrl, httpClient!, socketConnector: () {
+    //   return IOWebSocketChannel.connect(kSocketUrl).cast<String>();
+    // });
+  }
 
   @override
   Widget build(BuildContext context) {
