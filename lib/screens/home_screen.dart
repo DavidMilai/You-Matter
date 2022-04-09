0adba48996005f2b65a1e9e3f14423a2d44c6e00
DavidMilai <davidmilai67@gmail.com>
2022-04-09 15:48:20 +0300

    abi

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:web3dart/web3dart.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +11,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Client? httpClient;
+  Web3Client? web3Client;
+
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
